7c3d2c09f575932c21f164c5a94ce30385052891
Almaaz khan <khan.almaaz1055@gmail.com>
2024-06-27 19:20:20 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import { BackgroundGradientAnimationDemo } from "@/components/ui/BackgroundGradientAnimationDemo";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <BackgroundGradientAnimationDemo />
+    </div>
+  );
+};
+
+export default page;
